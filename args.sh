c374d23503bd364d668a2e384a1cf7459625b81b
chandra <chandraannem01@gmail.com>
2025-05-21 13:42:05 +0530

    chanfes

---
+++ args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2S
+
+echo "$person1::hello this ,e "
+echo "$person2::hello $person1 ,whee are y"
+echo "$person1::is thos group okay"
+echo "$person2::yes this group is okay"
